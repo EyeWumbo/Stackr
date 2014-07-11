440a2c1c308a3b4472e49359c4d1cae80bb0384f
EyeWumbo <sbzhang@uci.edu>
2014-07-11 14:19:23 -0700

    Initial commit

---
+++ Assets/scripts/BlockRow.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BlockRow : MonoBehaviour {
+
+	bool isActive = true;
+
+	public BlockRow(int numBlocks, float speed){
+
+	}
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
---
+++ Assets/scripts/Main.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Main : MonoBehaviour {
+
+	int[][] rowContainer = new int[12][7];
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//Apply update logic to BaseGame.
+//		foreach (Touch t in Input.touches){
+//			Debug.Log ("Screen was pressed");
+//		}
+		if(Touch.tapCount > 0){
+			Debug.Log ("SHIEEEET");
+		}
+	}
+}
